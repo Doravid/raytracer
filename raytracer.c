92d8ebff06e7a02d7a73065f7158439638a9c005
doravid <doravidmc@gmail.com>
2026-02-27 11:52:44 -0500

    add: Matricies, Matrix Add, Matrix Multiply

--- raytracer.c
+++ raytracer.c
@@ -35,8 +35,10 @@ Tuple tuple_cross(Tuple a, Tuple b);
 Canvas canvas(int width, int height);
 Tuple color(float red, float green, float blue);
 void write_pixel(Canvas *can, int x, int y, Tuple col);
-char *canvas_to_ppm(Canvas *can);
+char *canvas_to_ppm(Canvas *can, char *file_name);
 void write_ppm_to_file(char *data, int width, int height, char *file_name);
+bool mat_equal(int size, float mat_a[size][size], float mat_b[size][size]);
+void mat_mult(int size, float mat_a[size][size], float mat_b[size][size], float mat_out[size][size]);
 
 int main(int argc, char const *argv[])
 {
@@ -122,7 +124,7 @@ int main(int argc, char const *argv[])
     assert(tuple_equal(can.canvas[1900][1], color(1, 0.2, 0.3)));
 
     // Test write to ppm
-    const int size = 50;
+    const int size = 80;
     Canvas new_canvas = canvas(size, size);
     Tuple red = color(0.9, 0.3, 0.3);
 
@@ -133,9 +135,43 @@ int main(int argc, char const *argv[])
             new_canvas.canvas[i][j] = color(.6, i / 30.0, j / 30.0);
         }
     }
-    char *image = canvas_to_ppm(&new_canvas);
+    char *image = canvas_to_ppm(&new_canvas, "output.ppm");
+    // Test Physics Sim thing
+    Tuple black = color(0, 0, 0);
+    char filename[25];
+    Canvas physics_canvas = canvas(size, size);
+    Tuple position = point(0, 0, 0);
+    Tuple velocity = vector(5, 12, 0);
 
-        // Everything passes
+    Tuple acceleration = vector(-0.15, -1, 0);
+    const int MAX_STEPS = 30;
+    for (int i = 0; i < MAX_STEPS; ++i)
+    {
+
+        int x = (int)position.x;
+        int y = (int)position.y;
+
+        if (x >= 0 && x < size && y >= 0 && y < size)
+        {
+            physics_canvas.canvas[x][size - y - 1] = red;
+        }
+        position = tuple_add(position, velocity);
+        velocity = tuple_add(velocity, acceleration);
+    }
+    canvas_to_ppm(&physics_canvas, "physics.ppm");
+    // Test Matrix
+    float my_matrix[4][4] = {{1, 2, 3, 4}, {5.5, 6.5, 7.5, 8.5}, {9, 10, 11, 12}, {13.5, 14.5, 15.5, 16.5}};
+    float my_matrix2[4][4] = {{1, 2, 3, 4}, {5.5, 6.5, 7.5, 8.5}, {9, 10, 11, 12}, {13.5, 14.5, 15.5, 16.5}};
+    float my_matrix3[4][4] = {{1, 2, 3, 4}, {5.5, 6.5, 7.5, 8.5}, {9, 10, 11, 12}, {13.5, 14.5, 15.5, 16.54}};
+
+    assert(my_matrix[0][0] == 1);
+    assert(my_matrix[0][3] == 4);
+    assert(my_matrix[1][2] == 7.5);
+
+    assert(mat_equal(4, &my_matrix[0], &my_matrix2[0]));
+    assert(!mat_equal(4, &my_matrix[0], &my_matrix3[0]));
+
+    // Everything passes
     puts("All checks pass.");
 }
 
@@ -251,17 +287,8 @@ void write_pixel(Canvas *can, int x, int y, Tuple col)
 {
     (*can).canvas[x][y] = col;
 }
-void write_ppm_to_file(char *data, int width, int height, char *file_name)
-{
-    FILE *file = fopen(file_name, "wb");
-    char temp[50];
-    int header_len = sprintf(temp, "P6\n%d %d\n255\n", width, height);
 
-    fwrite(temp, 1, header_len, file);
-    fwrite(data, 1, (width * height * 3) + 1, file);
-    fclose(file);
-}
-char *canvas_to_ppm(Canvas *can)
+char *canvas_to_ppm(Canvas *can, char *file_name)
 {
     char *ppm_data = malloc(sizeof(char) * 3 * can->width * can->height + 1);
 
@@ -298,6 +325,41 @@ char *canvas_to_ppm(Canvas *can)
         }
     }
     pointer[0] = '\n';
-    write_ppm_to_file(ppm_data, can->width, can->height, "output.ppm");
+    FILE *file = fopen(file_name, "wb");
+    char temp[50];
+    int header_len = sprintf(temp, "P6\n%d %d\n255\n", can->width, can->height);
+
+    fwrite(temp, 1, header_len, file);
+    fwrite(ppm_data, 1, (can->width * can->height * 3) + 1, file);
+    fclose(file);
     return ppm_data;
+}
+
+bool mat_equal(int size, float mat_a[size][size], float mat_b[size][size])
+{
+    for (int i = 0; i < size; i++)
+    {
+        for (int j = 0; j < size; j++)
+        {
+            if (!equal(mat_a[i][j], mat_b[i][j]))
+            {
+                return false;
+            }
+        }
+    }
+    return true;
+}
+// THIS HAS SIDE EFFECTS,
+void mat_mult(int size, float mat_a[size][size], float mat_b[size][size], float mat_out[size][size])
+{
+    for (int i = 0; i < size; i++)
+    {
+        for (int j = 0; j < size; j++)
+        {
+            for (int k = 0; k < size; k++)
+            {
+                mat_out[i][j] = mat_a[i][k] * mat_b[k][j];
+            }
+        }
+    }
 }
